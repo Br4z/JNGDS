5fdf32a03bdc5045b5a6a79104598ccdabac8f0f
Sebastián <sebastianidrobo@hotmail.com>
2021-09-21 17:10:14 -0500

    Pruebita :(

--- script-coding/gamescript.js
+++ script-coding/gamescript.js
@@ -801,9 +801,9 @@ function posicionInactiva(nComodin){
 
 function nuevosComodines(){
   //Comodines Aqui
-  // const numeroComodin = 5;
-  const listaComodin = [0,1,2,3,4,5,6,7,4,5,6,7,4,5,6,7,0,1,2,3,0,1,2,3,4,5,6,7,4,5,6,7,4,5,6,7,0,1,2,3,0,1,2,3,8,4,5,6,7,4,5,6,7,4,5,6,7,0,1,2,3];
-  const numeroComodin = listaComodin[getRandom(0,length(listaComodin))];
+  const numeroComodin = 4;
+  //const listaComodin = [0,1,2,3,4,5,6,7,4,5,6,7,4,5,6,7,0,1,2,3,0,1,2,3,4,5,6,7,4,5,6,7,4,5,6,7,0,1,2,3,0,1,2,3,8,4,5,6,7,4,5,6,7,4,5,6,7,0,1,2,3];
+  //const numeroComodin = listaComodin[getRandom(0,length(listaComodin))];
   //const numeroComodin[getRandom(0,)]
   update(Mundo, Mundo.comodines[numeroComodin].tiempoActivo = getRandom(30,50));
   update(Mundo, Mundo.comodines[numeroComodin].x = getRandom(2,26));
